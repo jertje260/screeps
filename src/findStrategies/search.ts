862683a726dd393665b124f865461fd16717c8e4
jertje260 <jwbroekh@avans.nl>
2018-10-23 23:29:43 +0200

    improved search for containers

--- src/findStrategies/search.ts
+++ src/findStrategies/search.ts
@@ -1,10 +1,18 @@
 export class Search {
     public static SetNearestSource(creep: Creep, workingSuccess: boolean | null) {
-        let container = creep.room.find(FIND_STRUCTURES, { filter: (structure: StructureContainer) => structure.structureType === STRUCTURE_CONTAINER && structure.store.energy > 0 });
-        if (container.length === 0) {
-            console.log('No container with energy nearby');
+        let nearestContainer = creep.pos.findClosestByPath(FIND_STRUCTURES, { filter: (structure: StructureContainer) => structure.structureType === STRUCTURE_CONTAINER && structure.store.energy > 0 });
+        if (nearestContainer == null) {
+            let container = creep.room.find(FIND_STRUCTURES, { filter: (structure: StructureContainer) => structure.structureType === STRUCTURE_CONTAINER && structure.store.energy > 0 });
+            if (container.length === 0) {
+                console.log('No container with energy nearby');
+            } else {
+                creep.memory.source = container[0].id;
+                if (workingSuccess !== null) {
+                    creep.memory.working = workingSuccess;
+                }
+            }
         } else {
-            creep.memory.source = container[0].id;
+            creep.memory.source = nearestContainer.id;
             if (workingSuccess !== null) {
                 creep.memory.working = workingSuccess;
             }
